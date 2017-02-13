9bd21e050adbcf27658fb25821001ef69c8a3cd5
Dawid Krzus <dkrzus92@gmail.com>
2017-02-13 20:26:34 +0100

    Drugi projekt

---
+++ ArduinoOne/ArduinoTwo/ArduinoTwo.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoTwo.ino
+ Created:	2/13/2017 8:26:04 PM
+ Author:	Barlo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
