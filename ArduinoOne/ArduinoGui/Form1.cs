e917369b7df6665168b5cca4dc48d500879a2fbd
Dawid Krzus <dkrzus92@gmail.com>
2017-02-13 20:51:10 +0100

    GUI sample

---
+++ ArduinoOne/ArduinoGui/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ArduinoDriver.SerialProtocol;
+using ArduinoUploader.Hardware;
+
+namespace ArduinoGui {
+    public partial class Form1 : Form
+    {
+        private ArduinoDriver.ArduinoDriver driver;
+        public Form1() {
+            InitializeComponent();
+            driver = new ArduinoDriver.ArduinoDriver(ArduinoModel.UnoR3);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var request = new DigitalReadRequest();
+            driver.Send()
+        }
+    }
+}
